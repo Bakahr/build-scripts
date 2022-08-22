e298376bd39cb21b7cca566cf6db258da7db1ffa
Bakahr <bakahr@gmail.com>
2022-08-22 08:28:57 +0200

    moved installation part
    moved installation part to its own readme file.

--- win-setup.ps1
+++ win-setup.ps1
@@ -1,18 +1,3 @@
-# Description: Boxstarter script
-#
-# Install boxstarter:
-# 	. { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
-#
-# You might need to set: Set-ExecutionPolicy RemoteSigned
-#
-# Run this boxstarter by calling the following from an **elevated** command-prompt:
-# 	start http://boxstarter.org/package/nr/url?<URL-TO-RAW-GIST>
-# OR
-# 	Install-BoxstarterPackage -PackageName <URL-TO-RAW-GIST> -DisableReboots
-#
-# Learn more: http://boxstarter.org/Learn/WebLauncher
-#
-
 # Temporary
 Disable-UAC
 
